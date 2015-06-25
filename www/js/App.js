d8bae1302c74fe4d594b6b2daf06ef9b7a67d75a
dbktsbg <dbkdev50@gmail.com>
2015-06-25 11:25:58 -0400

    2

--- www/js/App.js
+++ www/js/App.js
@@ -11,7 +11,10 @@ function onDeviceReady()
 
     var mySessionDataDownloader = new SessionDataDownloader();
 
-    alert(mySessionDataDownloader.LoopbackTest("Hi Mom!"));
+    var SessionKey = "156373";
+    var SlideCount = 1;
+
+    mySessionDataDownloader.DownloadSessionSlides(SessionKey, SlideCount);
     
 }
 
--- www/js/SessionDataDownloder.js
+++ www/js/SessionDataDownloder.js
@@ -9,10 +9,50 @@ SessionDataDownloader =
 
         // ========== Methods =========
 
-        self.LoopbackTest = 
-            function ( InputData )
+        self.DownloadSessionSlides =
+            function 
+                (
+                    SessionKey,
+                    SlideCount
+                )
             {
-                return InputData;
+                var SlideImageFileURI = encodeURI("http://cloudvotepro1.blob.core.windows.net/sessions/v1/Sub069674/App20/Tem/Ses156373/Res/Slide5.JPG");
+
+                var DataStore = cordova.file.dataDirectory;
+                alert(DataStore);
+
+                var FileName = "Ses" + SessionKey + "Slide1.JPG";
+
+                self.DownloadFile
+                        (
+                            SlideImageFileURI,
+                            DataStore + FileName,
+                            alert("Success!"),
+                            alert("ERROR")
+                        );
+            }
+
+        self.DownloadFile = 
+            function
+                (
+                    SourceFileURL,
+                    DestinationFileFullPathName,
+                    SuccessCallback,
+                    ErrorCallback
+                )
+            {
+
+                var myFileTransfer = new FileTransfer();
+
+                myFileTransfer
+                    .download
+                    (
+                        targetSourceFileURL,
+                        targetDestinationFileFullPathName,
+                        SuccessCallback,
+                        ErrorCallback
+                    );
+
             }
 
     }
